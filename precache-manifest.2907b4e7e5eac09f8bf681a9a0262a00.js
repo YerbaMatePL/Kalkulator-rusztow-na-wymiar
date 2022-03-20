fbdcbf323cfde192e60a2665322aeeaadf2e70c6
Ewelina <ewela.m95@gmail.com>
2022-03-20 18:57:45 +0100

    Updates

---
+++ precache-manifest.2907b4e7e5eac09f8bf681a9a0262a00.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "20bcfff53a7bb7027cd8",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/css/main.16a78b27.chunk.css"
+  },
+  {
+    "revision": "20bcfff53a7bb7027cd8",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/js/main.5472a4d5.chunk.js"
+  },
+  {
+    "revision": "4260834a3fc8cd1d89a1",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/js/runtime~main.0746f3a4.js"
+  },
+  {
+    "revision": "23d240e9a9c2de28227e",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/js/2.83f79d9c.chunk.js"
+  },
+  {
+    "revision": "7b4b590765ba9cc9d7de",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/js/3.f1bbbdc7.chunk.js"
+  },
+  {
+    "revision": "04dde3e66b8cd9b7891a63c49fed4de9",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/media/ruszt_zeliwny.04dde3e6.png"
+  },
+  {
+    "revision": "da2cd62b0aa2738d094cfc84a9750aac",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/media/ruszt_zeliwny_okragly.da2cd62b.png"
+  },
+  {
+    "revision": "1d42f726022df344f5fe0c7ac9288f43",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/media/ruszt_zeliwny_wymiary.1d42f726.png"
+  },
+  {
+    "revision": "7c0358b0130b8359572bbc8eac3be9a2",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/media/ruszt_chromowany.7c0358b0.png"
+  },
+  {
+    "revision": "e1467a05dd53c12ea4f526903552b00e",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/media/ruszt_zeliwny_chromowy.e1467a05.png"
+  },
+  {
+    "revision": "6d9b732b93bf2163420a8367219a37f4",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/media/ruszt_z_podluznych_pretow.6d9b732b.png"
+  },
+  {
+    "revision": "38908098a1f6393e0fcea595f25a6c51",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/static/media/ruszt_kominkowy_na_wymiar.38908098.png"
+  },
+  {
+    "revision": "306ffcf23a9eae46803c90b12f34581d",
+    "url": "/REACT-Kalkulator-rusztow-na-wymiar/index.html"
+  }
+];
